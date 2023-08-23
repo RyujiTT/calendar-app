151ba29d1ebd0d5858c4e4e88e4e71cfac45105a
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-08-23 06:47:16 +0000

    [Add]CalendarView & PlansView/[Edit]PlansController

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,46 @@
 class PlansController < ApplicationController
   def index
+    @plans = Plan.all
+    @plan = Plan.new
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def destroy
+    @plan = Plan.find(params[:id])
+    if @plan.destroy
+      redirect_to plans_path, notice:"削除しました"
+    end
+  end
+
+  def show
+    @plan = Plan.find(params[:id])
+  end
+
+  def create
+    Plan.create(plan_parameter)
+    redirect_to plans_path
+  end
+
+
+  def edit
+    @plan = Plan.find(params[:id])
+  end
+
+  def update
+    @plan = Plan.find(params[:id])
+    if @plan.update(plan_parameter)
+      redirect_to plans_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def plan_parameter
+    params.require(:plan).permit(:title, :content, :start_time)
   end
 end
---
+++ db/migrate/20230823041310_create_plans.rb
@@ -0,0 +1,12 @@
+class CreatePlans < ActiveRecord::Migration[6.1]
+  def change
+    create_table :plans do |t|
+
+      t.string :title
+      t.text :content
+      t.datetime :start_time
+
+      t.timestamps
+    end
+  end
+end
